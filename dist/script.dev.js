8dcc5ff91ca89a5d026260bc7066aa21b306fb00
James Harrison <jamesebharrison@hotmail.com>
2021-09-28 12:39:14 +0100

    compiled files with script.js

---
+++ dist/script.dev.js
@@ -0,0 +1,40 @@
+"use strict";
+
+function handleCalculateGrade(event) {
+  var grade = event.target.value;
+
+  if (!grade) {
+    return;
+  }
+
+  grade = grade.toUpperCase();
+  var percentage;
+
+  switch (grade) {
+    case "A":
+      percentage = "90-100%";
+      break;
+
+    case "B":
+      percentage = "75-90%";
+      break;
+
+    case "C":
+      percentage = "50-75%";
+      break;
+
+    case "D":
+      percentage = "35-50%";
+      break;
+
+    case "E":
+      percentage = "10-35%";
+      break;
+
+    case "F":
+      percentage = "0-10%";
+      break;
+  }
+
+  alert("The percentage was " + percentage);
+}
