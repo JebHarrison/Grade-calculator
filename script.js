991487f3a20e266f866630af46cbd1c8213d08b7
James Harrison <jamesebharrison@hotmail.com>
2021-09-28 10:42:43 +0100

    added all grades to switch case

--- script.js
+++ script.js
@@ -7,6 +7,25 @@ function handleCalculateGrade(event){
         case "A":
             percentage = "90-100%";
             break;
+        case "B":
+            percentage = "75-90%";
+            break;
+        case "C":
+            percentage = "50-75%";
+            break;
+        case "D":
+            percentage = "35-50%";
+            break;
+        case "E":
+            percentage = "0-35%"; 
+            break;           
+
+
+
+
+
+
+
     }
 
 
